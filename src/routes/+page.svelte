44a2f8864923c0447e43fa6d60ff23534816020d
Cyprien <cypriengehu@example.com>
2025-10-29 11:42:02 +0100

    feat: insertion of tailwind and first version of the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
-<h1>DataGenerator</h1>
-<a href="/Liste">
-    <button>Génerateur de liste</button>
-</a>
-<a href="/Tableau">
-    <button>Génerateur de tableau</button>
-</a>
+<svelte:head>
+	<title>Accueil DataGenerator</title>
+</svelte:head>
+
+<h1 class="text-4xl p-3 font-bold text-indigo-700 text-center">DataGenerator</h1>
+<div class="flex flex-col items-center gap-9 text-3xl md:text-4xl lg:text-5xl pt-10">
+    <a href="/Liste">
+        <button class="border-2 border-indigo-500 bg-indigo-200 rounded-2xl p-2
+        hover:bg-indigo-500 hover:shadow-xl">
+            Génerateur de liste
+        </button>
+    </a>
+    <a href="/Tableau">
+        <button class="border-2 border-indigo-500 bg-indigo-200 rounded-2xl p-2
+        hover:bg-indigo-500 hover:shadow-xl">
+            Génerateur de tableau
+        </button>
+    </a>
+</div>
