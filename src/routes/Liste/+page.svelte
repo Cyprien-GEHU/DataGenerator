7d1ed241fa44ddcc74a11a08b8dd3f9da5eafd56
Cyprien <cypriengehu@example.com>
2025-10-29 20:44:20 +0100

    feat: first version of the generation of list (front end)

--- src/routes/Liste/+page.svelte
+++ src/routes/Liste/+page.svelte
@@ -37,25 +37,40 @@
   }
 </script>
 
-<header>
+<header class="p-2 bg-sky-200">
     <a href="/">Retour</a>
 </header>
 
-<h1>Génerateur de Liste</h1>
+<h1 class="text-4xl p-3 font-bold text-indigo-700 text-center">Génerateur de Liste</h1>
 
-<input bind:value={newList} placeholder="Nom de votre liste"/>
-<button on:click={createList}>Créer la liste</button>
+<div class="flex flex-col items-center gap-5 pt-3 pb-4
+md:flex-row md:pt-5 md:pl-3">
+  <input bind:value={newList} placeholder="Nom de votre liste"
+    class="border-2 border-indigo-300 w-5/6 text-center focus:outline-none focus:border-indigo-700
+    md:w-2/6"/>
+  <button on:click={createList} class="border-2 border-indigo-500 bg-indigo-200 rounded-2xl p-1
+          hover:bg-indigo-500 hover:shadow-xl w-9/10 md:p-0 md:w-1/6 lg:w-1/7">
+    Créer la liste
+  </button>
+</div>
 
 {#if Object.keys(lists).length == 0}
-    <h2>Vous n'avez pas encore de liste</h2>
+    <h2 class="text-center pt-5">Vous n'avez pas encore de liste !</h2>
 {:else}
-    <h2>Mes Listes:</h2>
+    <h2 class="underline font-bold p-3">Mes Listes:</h2>
 {/if}
-<ul>
+<ul class="flex flex-col items-center flex-wrap justify-center sm:flex-row lg:justify-evenly">
   {#each Object.keys(lists) as listName}
-    <li>
-      <a href={`/Liste/${listName}`}>{listName}</a>
-      <button on:click={() => deleteList(listName)}>Supprimer</button>
+    <li class="flex flex-col items-center m-3 p-3 gap-3 bg-indigo-300 rounded-2xl w-70 sm:w-4/10 lg:w-1/5
+    ">
+      <h3 class="font-bold">{listName}</h3>
+      <a href={`/Liste/${listName}`} class="border-2 border-indigo-500 bg-indigo-200 rounded-2xl w-3/4
+        hover:bg-indigo-500 hover:shadow-xl text-center"
+        >Regarder la liste</a>
+      <button on:click={() => deleteList(listName)} class="border-2 border-indigo-500 bg-indigo-200 rounded-2xl w-3/4
+        hover:bg-indigo-500 hover:shadow-xl">
+        Supprimer la liste
+      </button>
     </li>
   {/each}
 </ul>
